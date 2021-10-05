8fa5d0b5d4753d817d0c45662b18be17ee1a2595
isaac <joseisaacleal@gmail.com>
2021-10-04 23:41:43 -0500

    firebase parte1

--- app/src/main/java/com/example/chatuisapp/activities/LoginActivity.kt
+++ app/src/main/java/com/example/chatuisapp/activities/LoginActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.chatuisapp.R
 import kotlinx.android.synthetic.main.activity_login.*
+import android.content.Intent
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class LoginActivity : AppCompatActivity() {
 
+    private val auth =Firebase.auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,15 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun checkUser(){
+        val currentUser = auth.currentUser
 
+        if(currentUser != null){
+            val intent = Intent(this,ListOfChatsActivity::class.java)
+            intent.putExtra( "user",currentUser.email)
+            startActivity(intent)
+
+            finish()
+        }
     }
 
     private fun createUser(){
@@ -27,7 +41,17 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun loginUser(){
-
+        val email =usuario.text.toString()
+        val password =contraseña.text.toString()
+        auth.signInWithEmailAndPassword(email , password ).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                checkUser()
+            }else{
+                task.exception?.let{
+                    Toast.makeText(baseContext,it.message,Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 
 }
